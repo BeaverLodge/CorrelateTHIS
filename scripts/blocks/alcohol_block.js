e719fc29ec99b32bfa1422c961d5deec5012fe87
Nat Young <nryoung@thoughtworks.com>
2014-07-12 16:35:12 +0800

    changed unit calculation of alcohol and did other things

--- scripts/blocks/alcohol_block.js
+++ scripts/blocks/alcohol_block.js
@@ -6,38 +6,43 @@ ko.bindingHandlers.alcoholBlock = {
   },
   update: function (element, valueAccessor) {
     var value = ko.unwrap(valueAccessor());
-
     var diff = Math.abs(value.populationAMean - value.populationBMean);
-
     var data = [];
-    for (var i = 0; i < Math.floor(diff); i++)
-      data.push(1);
-    if (diff - Math.floor(diff) > 0)
-      data.push(diff - Math.floor(diff));
 
-    svg = d3.select(element).select("svg");
-    var updater = svg.selectAll(".cups")
-       .data(data);
+    for (var i = 0; i < Math.floor(diff); i++) {
+      data.push(1);
+    }
+
+    if (diff - Math.floor(diff) > 0) {
+      data.push(diff - Math.floor(diff));
+    }
     
+    svg = d3.select(element).select("svg");
+    var updater = svg.selectAll(".cup")
+     .data(data);
+
     updater.enter()
       .append("rect")
-      .attr("x", function(d, i) { return i * 15; } )
-      .attr("y", 10)
-      .attr("width", 10)
+      .attr("x", function(d, i) { return i * 70; } )
+      .attr("y", 80)
+      .attr("width", 50)
       .attr("height", 0)
-      .attr("class", "cups")
-      .attr("background", "pink");
+      .attr("class", "cup");
 
     updater
       .transition()
       .delay(function(d, i) { return i * 500; })
-      .attr("y", function(d) { return 10 - d * 10; } )
-      .attr("height", function(d) { return d * 10; } );
+      .attr("y", function(d) { return 80 - d * 80; } )
+      .attr("height", function(d) { return d * 80; } );
 
     updater.exit()
-        .transition()
-        .attr("y", 10)
-        .attr("height", 0)
-        .remove();
+      .transition()
+      .attr("y", 80)
+      .attr("height", 0)
+      .remove();
   }
 };
+
+
+
+
