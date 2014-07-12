bce6d7be3055a17c3920aeb596536667c7c37c58
Brad Ward <brad@groupmap.com>
2014-07-12 14:36:39 +0800

    Add alcohol block

---
+++ scripts/blocks/alcohol_block.js
@@ -0,0 +1,43 @@
+ko.bindingHandlers.alcoholBlock = {
+  init: function (element, valueAccessor) {
+    d3.select(element)
+      .append("svg")
+      .classed("block", true);
+  },
+  update: function (element, valueAccessor) {
+    var value = ko.unwrap(valueAccessor());
+
+    var diff = Math.abs(value.populationAMean - value.populationBMean);
+
+    var data = [];
+    for (var i = 0; i < Math.floor(diff); i++)
+      data.push(1);
+    if (diff - Math.floor(diff) > 0)
+      data.push(diff - Math.floor(diff));
+
+    svg = d3.select(element).select("svg");
+    var updater = svg.selectAll(".cups")
+       .data(data);
+    
+    updater.enter()
+      .append("rect")
+      .attr("x", function(d, i) { return i * 15; } )
+      .attr("y", 10)
+      .attr("width", 10)
+      .attr("height", 0)
+      .attr("class", "cups")
+      .attr("background", "pink");
+
+    updater
+      .transition()
+      .delay(function(d, i) { return i * 500; })
+      .attr("y", function(d) { return 10 - d * 10; } )
+      .attr("height", function(d) { return d * 10; } );
+
+    updater.exit()
+        .transition()
+        .attr("y", 10)
+        .attr("height", 0)
+        .remove();
+  }
+};
