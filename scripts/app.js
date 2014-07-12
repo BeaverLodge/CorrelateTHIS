59bf740c2f85528c44c7c1f37fd35d3100b2a8b8
Steph Hof <steph.hof@gmail.com>
2014-07-12 09:53:17 +0800

    Load up the questions in to a (not so?) sensible data structure

--- scripts/app.js
+++ scripts/app.js
@@ -4,4 +4,29 @@ $(function()
   masher.load(function() {
     masher.compareTwoPopulations({}, { Alcohol: '2' });
   });
+
+  console.log("Loading questions...");
+  questions = {};
+  d3.csv("../data/variabledefinitions.csv")
+    .row(function(d) { return d; })
+    .get(function(error, rows) {
+      _(rows).each(function(row) {
+        if (questions[row.Variable] == undefined) {
+          questions[row.Variable] = {answers: [{value: row.Value, label: row.Label}]};
+        } else {
+          questions[row.Variable].answers.push({value: row.Value, label: row.Label});
+        }
+      });
+    d3.csv("../data/variabledescriptions.csv")
+      .row(function(d) {return d; })
+      .get(function(error, rows) {
+        _(rows).each(function(row) {
+          if (questions[row.Variable]) {
+            questions[row.Variable]['question'] = row.Label;
+          }
+        });
+      console.log("Questions loaded...");
+      console.log(questions);
+      });
+    });
 });
