0a68ea4211d902e46542a45104a8b2f415b7b617
Brad Ward <brad@groupmap.com>
2014-07-12 14:20:42 +0800

    D3 drinking

--- scripts/blocks/smoking_block.js
+++ scripts/blocks/smoking_block.js
@@ -1,16 +1,46 @@
 ko.bindingHandlers.smokingBlock = {
   init: function (element, valueAccessor) {
-  	d3.select(element)
-  	  .append("svg")
-  	  .attr("width", 400)
-  	  .attr("height", 200);
+    d3.select(element)
+      .append("svg")
+      .attr("width", 400)
+      .attr("height", 200);
   },
   update: function (element, valueAccessor) {
-  	var value = ko.unwrap(valueAccessor());
-  	var valueA = value.populationAMean;
-  	var valueB = value.populationAMean;
+    var value = ko.unwrap(valueAccessor());
 
-  	svg = d3.select(element).select("svg")
-  	svg.style("background", "red")
+    var diff = Math.abs(value.populationAMean - value.populationBMean);
+
+    var data = [];
+    for (var i = 0; i < Math.floor(diff); i++)
+      data.push(1);
+    if (diff - Math.floor(diff) > 0)
+      data.push(diff - Math.floor(diff));
+
+    svg = d3.select(element).select("svg");
+    svg.style("background", "red");
+
+    var updater = svg.selectAll(".cups")
+       .data(data);
+    
+    updater.enter()
+      .append("rect")
+      .attr("x", function(d, i) { return i * 15; } )
+      .attr("y", 10)
+      .attr("width", 10)
+      .attr("height", 0)
+      .attr("class", "cups")
+      .attr("background", "pink");
+
+    updater
+      .transition()
+      .delay(function(d, i) { return i * 500; })
+      .attr("y", function(d) { return 10 - d * 10; } )
+      .attr("height", function(d) { return d * 10; } );
+
+    updater.exit()
+        .transition()
+        .attr("y", 10)
+        .attr("height", 0)
+        .remove();
   }
 };
