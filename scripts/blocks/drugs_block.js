6f57c982f7b2a0f1bf4354bca33532da7f94c7a6
Steph Hof <steph.hof@gmail.com>
2014-07-13 13:08:36 +0800

    Drugs are now more fashionable

--- scripts/blocks/drugs_block.js
+++ scripts/blocks/drugs_block.js
@@ -12,39 +12,27 @@ ko.bindingHandlers.drugsBlock = {
        .attr("fill", "#73B9F3");
 
     svg.append("text")
+      .classed("percentage", true)
+       .style("font-size", 70)
+       .style("fill", "#AB4429")
+       .attr("x", 240)
+       .attr("y", 80);
+
+    svg.append("text")
        .classed("description", true)
        .style("font-size", 28)
-       .style("font-weight", "bold")
-       .style("fill", "#5D5124")
-       .attr("x", 440)
-       .attr("y", 80);
+       .style("fill", "#AB4429")
+       .attr("x", 430)
+       .attr("y", 120)
+       .text(" chance that you do illicit drugs");
 
     svg.append("text")
        .classed("tagline", true)
        .style("font-size", 16)
-       .style("fill", "#5D5124")
-       .attr("x", 430)
+       .style("fill", "#AB4429")
+       .style("font-weight", "bold")
+       .attr("x", 490)
        .attr("y", 160);
-
-    svg.append("g")
-       .classed("pop-pill", true);
-
-    var popPill = svg.select(".pop-pill");
-    popPill.append("ellipse")
-        .attr("cx", 93).attr("cy", 86)
-        .attr("rx", 70).attr("ry", 70)
-        .attr("stroke", "#5D5124").attr("stroke-width", 11)
-        .attr("fill", "#858930");
-    popPill.append("line")
-        .attr("stroke", "#A1AA35").attr("stroke-width", 9)
-        .attr("x1", 91).attr("x2", 91)
-        .attr("y1", 21).attr("y2", 151);
-    popPill.append("line")
-        .attr("stroke", "#5D5124").attr("stroke-width", 9)
-        .attr("x1", 97).attr("x2", 97)
-        .attr("y1", 21).attr("y2", 151);
-
-    popPill.attr("transform", "translate(700 110) scale(0.4)");
 
     svg.append("g")
        .classed("filter-pill", true);
@@ -59,10 +47,6 @@ ko.bindingHandlers.drugsBlock = {
         .attr("stroke", "#A1AA35").attr("stroke-width", 9)
         .attr("x1", 91).attr("x2", 91)
         .attr("y1", 21).attr("y2", 151);
-    filterPill.append("line")
-        .attr("stroke", "#5D5124").attr("stroke-width", 9)
-        .attr("x1", 97).attr("x2", 97)
-        .attr("y1", 21).attr("y2", 151);
 
     filterPill.attr("transform", "translate(60 10)");
   },
@@ -73,8 +57,8 @@ ko.bindingHandlers.drugsBlock = {
 
     var svg = d3.select(element).select("svg");
 
-    var text = svg.select(".description")
-                  .text(Math.round(filterAverage * 100) + "% chance of doing illicit drugs");
+    var text = svg.select(".percentage")
+                  .text(Math.round(filterAverage * 100) + "%");
     var difference = Math.round(filterAverage * 100) - Math.round(populationAverage * 100);
     var descriptor = (difference < 0) ? "less" : "more"
     var text = svg.select(".tagline")
