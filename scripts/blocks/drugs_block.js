cb0cc7e8fe446942204e2afb092dd5d15f8e5045
Steph Hof <steph.hof@gmail.com>
2014-07-13 12:20:04 +0800

    Drugs are in style

--- scripts/blocks/drugs_block.js
+++ scripts/blocks/drugs_block.js
@@ -9,12 +9,22 @@ ko.bindingHandlers.drugsBlock = {
        .classed("background", true)
        .attr("height", 180)
        .attr("width", 800)
-       .attr("fill", "#EBD2FF");
+       .attr("fill", "#73B9F3");
 
     svg.append("text")
        .classed("description", true)
-       .attr("x", 400)
-       .attr("y", 40);
+       .style("font-size", 28)
+       .style("font-weight", "bold")
+       .style("fill", "#5D5124")
+       .attr("x", 440)
+       .attr("y", 80);
+
+    svg.append("text")
+       .classed("tagline", true)
+       .style("font-size", 16)
+       .style("fill", "#5D5124")
+       .attr("x", 430)
+       .attr("y", 160);
 
     svg.append("g")
        .classed("pop-pill", true);
@@ -23,18 +33,14 @@ ko.bindingHandlers.drugsBlock = {
     popPill.append("ellipse")
         .attr("cx", 93).attr("cy", 86)
         .attr("rx", 70).attr("ry", 70)
-        .attr("stroke", "#ffaa56").attr("stroke-width", 11)
-        .attr("fill", "#ffd4aa");
+        .attr("stroke", "#5D5124").attr("stroke-width", 11)
+        .attr("fill", "#858930");
     popPill.append("line")
-        .attr("stroke", "#fff3e8").attr("stroke-width", 9)
+        .attr("stroke", "#5D5124").attr("stroke-width", 9)
         .attr("x1", 97).attr("x2", 97)
         .attr("y1", 21).attr("y2", 151);
-    popPill.append("line")
-        .attr("stroke", "#ffaa56").attr("stroke-width", 9)
-        .attr("x1", 91).attr("x2", 91)
-        .attr("y1", 21).attr("y2", 151);
 
-    popPill.attr("transform", "translate(100 10)");
+    popPill.attr("transform", "translate(700 110) scale(0.4)");
 
     svg.append("g")
        .classed("filter-pill", true);
@@ -43,36 +49,45 @@ ko.bindingHandlers.drugsBlock = {
     filterPill.append("ellipse")
         .attr("cx", 93).attr("cy", 86)
         .attr("rx", 70).attr("ry", 70)
-        .attr("stroke", "#007f7f").attr("stroke-width", 11)
-        .attr("fill", "#00bfbf");
+        .attr("stroke", "#5D5124").attr("stroke-width", 11)
+        .attr("fill", "#858930");
     filterPill.append("line")
-        .attr("stroke", "#00ffff").attr("stroke-width", 9)
-        .attr("x1", 91).attr("x2", 91)
-        .attr("y1", 21).attr("y2", 151);
-    filterPill.append("line")
-        .attr("stroke", "#007f7f").attr("stroke-width", 9)
+        .attr("stroke", "#5D5124").attr("stroke-width", 9)
         .attr("x1", 97).attr("x2", 97)
         .attr("y1", 21).attr("y2", 151);
 
-    filterPill.attr("transform", "translate(500 10)");
+    filterPill.attr("transform", "translate(60 10)");
   },
   update: function (element, valueAccessor) {
     var value = ko.unwrap(valueAccessor());
-    var populationAverage = value.populationAMean;
-    var filterAverage = value.populationBMean;
+    var filterAverage = value.populationAMean;
+    var populationAverage = value.populationBMean;
 
     var svg = d3.select(element).select("svg");
 
-
-    console.log("filter mean: " + value.populationAMean);
-    console.log("populaion average: " + value.populationBMean);
-    var diff = Math.abs(value.populationAMean - value.populationBMean);
-    // Are XX% more likely to have been a recent user of illicit drugs
-    console.log(diff);
-
-    var data = [];
-
     var text = svg.select(".description")
-                  .text("...on average. Do STUFF");
+                  .text(Math.round(filterAverage * 100) + "% chance of doing illicit drugs");
+    var difference = Math.round(filterAverage * 100) - Math.round(populationAverage * 100);
+    var descriptor = (difference < 0) ? "less" : "more"
+    var text = svg.select(".tagline")
+                  .text("That's " + Math.abs(difference) + "% " + descriptor +" than the average of " + Math.round(populationAverage * 100) + "%");
+
+    var arc = d3.svg.arc().outerRadius(70).innerRadius(0);
+    var pie = d3.layout.pie().sort(null).value(function(d) { return d.value; });
+
+    var popPieGroup = d3.select(element).select(".pop-pill");
+    var popData = [{ value: populationAverage, label: 'yes'}, { value: 1-populationAverage, label: 'no'}];
+
+    var popGroup = popPieGroup.selectAll(".arc").data(pie(popData));
+    popGroup.enter().append("path").attr("d", arc).attr("class", "arc");
+    popGroup.attr("d", arc).attr("transform", "translate(93, 86)");
+
+    var filterPieGroup = d3.select(element).select(".filter-pill");
+    var filterData = [{ value: filterAverage, label: 'yes'}, { value: 1-filterAverage, label: 'no'}];
+
+    var filterGroup = filterPieGroup.selectAll(".arc").data(pie(filterData));
+    filterGroup.enter().append("path").attr("d", arc).attr("class", "arc");
+    filterGroup.attr("d", arc).attr("transform", "translate(93, 86)");
+
   }
 };
