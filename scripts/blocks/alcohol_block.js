739a9f1e43e812496494fac3b59b2bd91cdb994d
Nat Young <nryoung@thoughtworks.com>
2014-07-13 14:26:24 +0800

    doing more science

--- scripts/blocks/alcohol_block.js
+++ scripts/blocks/alcohol_block.js
@@ -9,53 +9,87 @@ ko.bindingHandlers.alcoholBlock = {
        .classed("background", true)
        .attr("height", 180)
        .attr("width", 800)
-       .attr("fill", "#5C3B5F");
+       .attr("fill", "#88c2ea");
+    svg.append('g')
+      .attr("transform", "translate(50,20)")
+      .classed("glasses-group", true);
+    svg.append('g')
+      .attr("transform", "translate(50,20)")
+      .classed("contents-group", true);
+    svg.append('g')
+      .attr("transform", "translate(50,10)")
+      .classed("bubbles-group", true);
+    svg.append('text')
+      .style("font-size", 20)
+      .style("fill", "#AB4429")
+      .attr("x", 400)
+      .attr("y", 160)
+      .classed('description', true);      
   },
   update: function (element, valueAccessor) {
     var value = ko.unwrap(valueAccessor());
     var diff = Math.abs(value.populationAMean - value.populationBMean);
     var data = [];
-    var glassData = [];
+    var glassData = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
     for (var i = 0; i < Math.floor(diff); i++) {
       data.push(1);
-      glassData.push(1);
     }
     if (diff - Math.floor(diff) > 0) {
       data.push(diff - Math.floor(diff));
-      glassData.push(1);
     }
     
-    var msDelay = 500;
+    var msDelay = 200;
     var glassHeight = 80;
     var glassWidth = 50;
     var bonusVar = 100;
+    var variable = 50;
 
     var svg = d3.select(element).select("svg");
 
-    var glassesGroup = svg.append('g')
-      .attr("transform", "translate(20,20)");
+    var text = "average freuency of alcohol consumption";
+    var description = svg.select('.description')
+      .text(text);
+
+    var glassesGroup = svg.select(".glasses-group");
     var glasses = glassesGroup.selectAll(".glass")
       .data(glassData);
 
     glasses.enter()
-      .append("rect")
-      .attr("class", "glass")
-      .attr('fill', '#ffffff')
-      .attr('stroke', '#dbc5c5')
-      .attr('stroke-width', 2)
-      .attr("width", glassWidth)
-      .attr("height", glassHeight)
-      .attr("x", function(d, i) { return i * (glassWidth + 10); } );
+      .append("g")
+      .classed("glass", true)
+      .each(function (d, i) {
+        var glass = d3.select(this);      
+        glass.append("rect")
+        .attr('fill', '#ffffff')
+        .attr('stroke', '#dbc5c5')
+        .attr('stroke-width', 2)
+        .attr("width", glassWidth)
+        .attr("height", glassHeight)
+        .attr("x", i * (glassWidth + variable));
+        glass.append('text')
+          .text(d)
+          .attr('x', i * (glassWidth + variable) + (glassWidth / 2))
+          .attr('y', glassHeight + 20)
+          .attr('text-anchor', 'middle')
+          .attr('fill', '#71652b');
+        glass.append('rect')
+          .attr('x', i * (glassWidth + variable) + glassWidth)
+          .attr('y', glassHeight / 4)
+          .attr('fill', 'transparent')
+          .attr('stroke', '#dbc5c5')
+          .attr('stroke-width', 4)
+          .attr("width", glassWidth / 4)
+          .attr("height", glassHeight / 2);          
+      })
 
-    var contentsGroup = svg.append('g')
-      .attr("transform", "translate(20,20)");
+    var contentsGroup = svg.select('.contents-group');
     var contents = contentsGroup.selectAll(".contents")
       .data(data);
 
     contents.enter()
       .append("rect")
       .attr("class", "contents")
-      .attr("x", function(d, i) { return i * (glassWidth + 10); } )
+      .attr("x", function(d, i) { return i * (glassWidth + variable); } )
       .attr("y", glassHeight - 8)
       .attr("width", glassWidth)
       .attr("height", 0)
@@ -64,6 +98,7 @@ ko.bindingHandlers.alcoholBlock = {
 
     contents.transition()
       .delay(function(d, i) { return i * msDelay; })
+      .duration(1000)
       .attr("y", function(d) { return glassHeight - d * glassHeight + 8; } )
       .attr('fill-opacity', '0.9')
       .attr("height", function(d) { return d * glassHeight - 10; } );
@@ -74,8 +109,7 @@ ko.bindingHandlers.alcoholBlock = {
       .attr("height", 0)
       .remove();
     
-    var bubbleGroup = svg.append('g')
-      .attr("transform", "translate(20,10)");
+    var bubbleGroup = svg.select('.bubbles-group');
     var bubblesData = _.reject(data, function(d) { return d < 1; });  
     var bubbles = bubbleGroup.selectAll(".bubble")
       .data(bubblesData);
@@ -83,7 +117,7 @@ ko.bindingHandlers.alcoholBlock = {
     bubbles.enter()
       .append("rect")
       .attr("class", "bubble")
-      .attr("x", function(d, i) { return i * (glassWidth + 10); } )
+      .attr("x", function(d, i) { return i * (glassWidth + variable); } )
       .attr("y", function(d) { return glassHeight - d * glassHeight + 10; })
       .attr("width", glassWidth)
       .attr("height", 0)
